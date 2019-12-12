9beb2b63cbf7471ef95a7d60b2c8b15f9c944edd
Evgeny Gulyaev <evgeny.gulyaev@fusion-team.com>
2019-12-12 11:18:45 +0300

    Updates

---
+++ precache-manifest.5bb6dcadf4f8eacc6419f0e0259d5e2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525389ce4ddf761c795080a8c96974a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af6d7508a96ae6389e31",
+    "url": "/static/css/2.ca79aa8d.chunk.css"
+  },
+  {
+    "revision": "af6d7508a96ae6389e31",
+    "url": "/static/js/2.e07a28d0.chunk.js"
+  },
+  {
+    "revision": "104746204a4fed787d29ad6713a376b6",
+    "url": "/static/js/2.e07a28d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89b7690ac4d7d0f71e3e",
+    "url": "/static/js/main.68ccb0dc.chunk.js"
+  },
+  {
+    "revision": "d723f2e3251c973fb873",
+    "url": "/static/js/runtime-main.77d3be62.js"
+  }
+]);
